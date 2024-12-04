da698e8d13a5c31180aa7c59b9dda145ce318167
IlhamSetiaji <ilham.ahmadz18@gmail.com>
2024-12-04 19:57:12 +0700

    feat: added consumer to run forever

--- rabbitmq/consumer.go
+++ rabbitmq/consumer.go
@@ -8,21 +8,22 @@ import (
 
 type MessageHandler func(delivery amqp091.Delivery)
 
-func ConsumeMessages(queue string, handler MessageHandler) error {
+func ConsumeMessages(queue string, handler MessageHandler) {
 	ch := GetChannel()
 	defer ch.Close()
 
 	msgs, err := ch.Consume(queue, "", true, false, false, false, nil)
 	if err != nil {
 		log.Printf("Failed to consume messages from queue: %v", err)
-		return err
+		return
 	}
 
+	forever := make(chan bool)
 	go func() {
 		for d := range msgs {
 			handler(d)
 		}
 	}()
 	log.Printf("Listening for messages on queue: %s", queue)
-	return nil
+	<-forever
 }
